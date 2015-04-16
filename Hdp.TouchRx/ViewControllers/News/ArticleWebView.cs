bc795484825db7efebddc43a462d15f8f6a39139
Umur Gedik <umurgdk@gmail.com>
2015-04-17 00:58:24 +0300

    TouchRx - Clear nonsense comments

--- Hdp.TouchRx/ViewControllers/News/ArticleWebView.cs
+++ Hdp.TouchRx/ViewControllers/News/ArticleWebView.cs
@@ -16,9 +16,6 @@ namespace Hdp.TouchRx.ViewControllers.News
         public void WebViewDidLoad (object sender, EventArgs e)
         {
             var css = File.ReadAllText ("WebHelpers/ArticleBody.css").Replace('\n', ' ');
-//            NSString cssPath = new NSString(NSBundle.MainBundle.PathForResource("WebHelpers/ArticleBody", ".css"));
-//            NSString cssPath = path.AppendPathComponent(new NSString("WebHelpers/ArticleBody.css")); 
-
             var js = @"
             var css = '" + css + @"'
             var head = document.head || document.getElementsByTagName('head')[0];
