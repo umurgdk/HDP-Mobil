bf64395325a4d666f5bbd2078003fb44cf34f0bd
Umur Gedik <umurgdk@gmail.com>
2015-04-25 01:45:08 +0300

    Android is complete yiha!

---
+++ Hdp.Droid/Fragments/BaseLoadingFragment.cs
@@ -0,0 +1,74 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Android.Graphics;
+
+using ReactiveUI;
+using ReactiveUI.AndroidSupport;
+using Hdp.CoreRx.ViewModels;
+using Hdp.CoreRx.ViewModels.Events;
+
+namespace Hdp.Droid.Fragments
+{
+    public abstract class BaseLoadingFragment<TLoadingViewModel> : ReactiveUI.AndroidSupport.ReactiveFragment<TLoadingViewModel>
+        where TLoadingViewModel : ReactiveObject, ILoadingViewModel
+    {
+        public View ContentView { get; private set; }
+        public Color BackgroundColor { get; protected set; }
+
+        private View _loadingView;
+
+        public override void OnCreate (Bundle savedInstanceState)
+        {
+            base.OnCreate (savedInstanceState);
+
+            BackgroundColor = Color.ParseColor ("#EEEEEE");
+        }
+
+        public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            RelativeLayout layout = new RelativeLayout (container.Context);
+            layout.LayoutParameters = new ViewGroup.LayoutParams (ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent);
+            layout.SetBackgroundColor (Color.ParseColor ("#EEEEEE"));
+            layout.SetGravity (GravityFlags.Center);
+
+            ContentView = OnCreateContentView (inflater, container, savedInstanceState);
+            ContentView.Visibility = ViewStates.Gone;
+
+            _loadingView = inflater.Inflate (Resource.Layout.LoadingLayout, null);
+
+            layout.AddView (_loadingView);
+            layout.AddView (ContentView);
+
+            return layout;
+        }
+
+        public override void OnViewCreated (View view, Bundle savedInstanceState)
+        {
+            base.OnViewCreated (view, savedInstanceState);
+
+            this.OneWayBind (ViewModel,
+                x => x.IsLoading, 
+                x => x._loadingView.Visibility,
+                x => x ? ViewStates.Visible : ViewStates.Gone);
+
+            this.OneWayBind (ViewModel,
+                x => x.IsLoading,
+                x => x.ContentView.Visibility,
+                x => x ? ViewStates.Gone : ViewStates.Visible);
+        }
+
+        protected abstract View OnCreateContentView (LayoutInflater inflater, ViewGroup container, Bundle savedInstance);
+    }
+}
+
---
+++ Hdp.Droid/Fragments/EventsFragment.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+using ReactiveUI.AndroidSupport;
+using Hdp.CoreRx.ViewModels.Events;
+using ReactiveUI;
+using Android.Graphics;
+using Android.Support.V4.Widget;
+
+namespace Hdp.Droid.Fragments
+{
+    public class EventsFragment : BaseLoadingFragment<EventsViewModel>
+    {
+        ReactiveListAdapter<EventItemViewModel> _eventsAdapter;
+
+        ListView _listView;
+        SwipeRefreshLayout _refreshLayout;
+
+        public EventsFragment (EventsViewModel viewModel)
+        {
+            ViewModel = viewModel;
+
+            _eventsAdapter = new ReactiveListAdapter<EventItemViewModel> (ViewModel.EventItems, CreateItemView, InitItemView);
+        }
+
+        public View CreateItemView (EventItemViewModel itemViewModel, ViewGroup viewGroup)
+        {
+            var inflator = LayoutInflater.From (viewGroup.Context);
+            return inflator.Inflate (Resource.Layout.EventItemLayout, null);
+        }
+
+        public void InitItemView (EventItemViewModel itemViewModel, View view)
+        {
+            var date = view.FindViewById<TextView> (Resource.Id.eventDate);
+            var title = view.FindViewById<TextView> (Resource.Id.eventTitle);
+            var time = view.FindViewById<TextView> (Resource.Id.eventTime);
+            var location = view.FindViewById<TextView> (Resource.Id.eventLocation);
+
+            date.Text = itemViewModel.DateText;
+            title.Text = itemViewModel.Title;
+            time.Text = itemViewModel.TimeText;
+            location.Text = itemViewModel.Location;
+        }
+
+        protected override View OnCreateContentView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = inflater.Inflate (Resource.Layout.EventsLayout, null);
+            view.SetBackgroundColor (Color.ParseColor ("#FFFFFF"));
+
+            _refreshLayout = view.FindViewById<SwipeRefreshLayout> (Resource.Id.swipeContainer);
+            _refreshLayout.Refresh += (sender, e) => ViewModel.FetchNewEvents.Execute(null);
+
+            ViewModel.FetchNewEvents.IsExecuting.BindTo (_refreshLayout, x => x.Refreshing);
+
+            _listView = view.FindViewById<ListView> (Resource.Id.eventsList);
+            _listView.Divider = null;
+            _listView.DividerHeight = 0;
+            _listView.Adapter = _eventsAdapter;
+
+            return view;
+        }
+    }
+}
+
