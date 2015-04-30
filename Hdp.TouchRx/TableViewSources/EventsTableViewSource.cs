5fe944137adc284812d1ce0c23785dddcb4eb5f2
Umur Gedik <umurgdk@gmail.com>
2015-04-30 14:19:08 +0300

    Hdp.TouchRx - EventsTableView row heights cached

--- Hdp.TouchRx/TableViewSources/EventsTableViewSource.cs
+++ Hdp.TouchRx/TableViewSources/EventsTableViewSource.cs
@@ -5,6 +5,7 @@ using Hdp.TouchRx.ViewControllers.Events;
 using ObjCRuntime;
 using UIKit;
 using CoreGraphics;
+using System.Collections.Generic;
 
 namespace Hdp.TouchRx.TableViewSources
 {
@@ -13,6 +14,7 @@ namespace Hdp.TouchRx.TableViewSources
         public static readonly nfloat estHeight = EventItemViewCell.EstimatedHeight;
 
         public static readonly EventItemViewCell demoCell = EventItemViewCell.Create();
+        private Dictionary<int, nfloat> _cellSizeCache = new Dictionary<int, nfloat>();
 
         public EventsTableViewSource (UIKit.UITableView tableView, IReactiveNotifyCollectionChanged<EventItemViewModel> collection)
             : base (tableView, collection, EventItemViewCell.Key, estHeight)
@@ -28,11 +30,20 @@ namespace Hdp.TouchRx.TableViewSources
 
         public override nfloat GetHeightForRow (UIKit.UITableView tableView, Foundation.NSIndexPath indexPath)
         {
+            nfloat cachedSize = 0.0f;
             var viewModel = (EventItemViewModel)ItemAt (indexPath);
+
+            if (_cellSizeCache.TryGetValue(viewModel.Id, out cachedSize))
+            {
+                return cachedSize;
+            }
 
             demoCell.ViewModel = viewModel;
             CGSize size = demoCell.ContentView.SystemLayoutSizeFittingSize (UIView.UILayoutFittingCompressedSize);
-            return size.Height + 1;
+            var height = size.Height + 1;
+
+            _cellSizeCache.Add (viewModel.Id, height);
+            return height;
         }
     }
 }
